2334ee8f4db89cf2645e44b50f0f79909b4c0cda
louradoua <alou2000.78@gmail.com>
2021-02-23 11:34:22 +0100

    Commit Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       /* BDF maBDF = new BDF();
+        System.out.println(maBDF);
+        maBDF.ajoutBDF("F4");
+        System.out.println(maBDF);*/
+
+        String[]t = {"c1","c2","","","","Concl"};
+        Regle R = new Regle(t);
+        System.out.println(R);
+
+
     }
 }
---
+++ src/com/company/Regle.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Regle {
+    private String [] schema;
+    private String [] valeurs;
+
+    public Regle(String[]val){
+        schema = new String[5];
+        valeurs = new String[5];
+
+        for (int i=0; i<5; i++){
+            if (i<4) schema[i] = "Condition " + i;
+            else schema[i] = "Conclusion";
+            valeurs[i] = val[i];
+        }
+    }
+
+    public String[] getSchema() {
+        return schema;
+    }
+
+    public String[] getValeurs() {
+        return valeurs;
+    }
+
+    @Override
+    public String toString() {
+        return "Regle{" +
+                "schema=" + Arrays.toString(schema) +
+                ", valeurs=" + Arrays.toString(valeurs) +
+                '}';
+    }
+}

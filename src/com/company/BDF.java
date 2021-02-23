73237d5fa712a535fdb96cf6bb380c0110e076ad
louradoua <alou2000.78@gmail.com>
2021-02-23 11:16:48 +0100

    Initial commit

---
+++ src/com/company/BDF.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BDF {
+    private ArrayList<String> contenu;
+    private int taille;
+
+    public BDF(){
+        String fait="";
+        contenu = new ArrayList<String>();
+        Scanner entree =  new Scanner(System.in);
+        System.out.println("Saisie des faits (terminer par la fin)");
+        while (!fait.equals("fin")){
+            System.out.println("fait :");
+            fait = entree.nextLine();
+            if (!fait.equals("fin")) contenu.add(fait);
+        }
+        taille = contenu.size();
+    }
+
+
+    public ArrayList<String> getContenu() {
+        return contenu;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void ajoutBDF(String fait){
+        contenu.add(fait);
+        taille += 1;
+    }
+
+    @Override
+    public String toString() {
+        return "BDF{" +
+                "contenu=" + contenu +
+                ", taille=" + taille +
+                '}';
+    }
+}
